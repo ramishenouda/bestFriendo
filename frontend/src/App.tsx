83b3a247edf61b067147f5ed891a248b37fe938a
ramshenouda <rami@codinglab.com>
2022-11-21 14:55:55 +0000

    home mobile responsive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import type { Component } from 'solid-js';
 import Footer from './components/footer';
-import Home from './components/home/home';
+import { Home, HomeSmallScreens } from './components/home/home';
 import Navbar from './components/navbar';
 
 const App: Component = () => {
   return (
     <div class="lg:px-20 px-5">
       <Navbar />
-      <Home />
+      <div class="hidden sm:block">
+        <Home />
+      </div>
+      <div class="block sm:hidden">
+        <HomeSmallScreens />
+      </div>
       <div class="footer">
         <Footer />
       </div>
--- frontend/src/components/home/home.tsx
+++ frontend/src/components/home/home.tsx
@@ -3,14 +3,14 @@ import './home.css';
 
 import background from '../../assets/home-background.svg';
 
-const Home: Component = () => {
+export const Home: Component = () => {
   return (
     <div class="flex lg:flex-row flex-col items-center justify-center">
       <div>
         <p class="text-primary font-semibold stranger-text text-[40px]">
-          From a complete stranger to a{' '}
+          From a complete stranger to a
           <span class="relative">
-            BestFriendo <span class="absolute no-select top-[26px] left-0 text-white absolute-text">BestFriendo</span>
+            BestFriendo <span class="no-select top-[26px] left-0 text-white absolute-text">BestFriendo</span>
           </span>
         </p>
         <div class="mt-20">
@@ -21,7 +21,7 @@ const Home: Component = () => {
               Text
             </button>
           </div>
-          <p>Note: Your video is monitored, please keep it clean</p>
+          <p class="text-primary">Note: Your video is monitored, please keep it clean</p>
         </div>
       </div>
       <div>
@@ -31,4 +31,32 @@ const Home: Component = () => {
   );
 };
 
-export default Home;
+export const HomeSmallScreens: Component = () => {
+  return (
+    <div class="flex lg:flex-row flex-col items-center justify-center">
+      <div>
+        <img src={background} />
+      </div>
+      <div>
+        <p class="text-primary text-center font-semibold stranger-text text-2xl">
+          From a complete stranger to a
+          <span class="relative">
+            BestFriendo <span class="no-select top-[16px] left-0 text-white absolute-text">BestFriendo</span>
+          </span>
+        </p>
+        <div class="mt-20">
+          <p class="text-primary font-medium text-center  text-2xl">Connect with a friendo:</p>
+          <div class="mb-4">
+            <button class="bg-primary text-white font-medium text-2xl lg:text-3xl rounded-xl  w-[150px] lg:w-[200px] py-3 mr-1">
+              Video
+            </button>
+            <button class="border-primary border-2 text-primary font-medium text-2xl lg:text-3xl rounded-xl  w-[150px] lg:w-[200px] py-3 ml-1">
+              Text
+            </button>
+          </div>
+          <p class="text-primary text-center my-20">Note: Your video is monitored, please keep it clean</p>
+        </div>
+      </div>
+    </div>
+  );
+};
